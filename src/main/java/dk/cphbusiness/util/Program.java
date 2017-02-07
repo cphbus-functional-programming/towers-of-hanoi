4955c6bc3a0a6277a764b1403f1379e2e7108a0c
Anders Kalhauge <anders@kalhauge.dk>
2017-02-07 16:00:56 +0100

    reverse

--- src/main/java/dk/cphbusiness/util/Program.java
+++ src/main/java/dk/cphbusiness/util/Program.java
@@ -2,11 +2,23 @@ package dk.cphbusiness.util;
 
 public class Program {
 
+
+  public static Path<String> reverse(Path<String> list) {
+    return reverse(list, null);
+    }
+  
+  private static Path<String> reverse(Path<String> rest, Path<String> reversed) {
+    if (rest == null) return reversed;
+    return reverse(rest.getRest(), new LinkedPath(rest.getFirst(), reversed));
+    }
+  
   public static void main(String[] args) {
     Path<String> list = new LinkedPath("Anders", "Bente", "Christine", "Dorte", "Erik");
-    while (list != null) {
-      System.out.println(list.getFirst());
-      list = list.getRest();
+    
+    Path<String> reversed = reverse(list);
+
+    for (Path<String> p = reversed; p != null; p = p.getRest()) {
+      System.out.println(p.getFirst());
       }
     
     }
